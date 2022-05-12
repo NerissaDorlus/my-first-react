b43fd25b7065ea548a86f8ead0ea46ef5265264b
Nerissa Dorlus <nerissadorlus@Tdas-Pro-3.local>
2022-05-12 16:25:05 -0400

    created a counter that counts clicks on a button incrementing when pressing + and decreasing when - is clicked

--- src/App.js
+++ src/App.js
@@ -2,13 +2,22 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0); /*same as let count = 0 */
+  const [firstName, setFirstName] = useState("Skye");
   return (
     <>
       <div className="App">
         <header className="App-header">
           <h1>You clicked the button {count} times.</h1>
-          <button onClick={() => count++}>Click Me!</button>
+          <button onClick={() => setCount(count + 1)}>+</button>
+          <button onClick={() => setCount(0)}>RESET</button>
+          {/* <button onClick={() => setCount(count ? count - 1 : 0)}>-</button> */}
+          <button onClick={() => setCount(Math.max(count - 1, 0))}>-</button>
+          <section>
+            <h2 onClick={() => setFirstName(firstName + "Elie")}>
+              Hello {firstName}.
+            </h2>
+          </section>
         </header>
       </div>
     </>
